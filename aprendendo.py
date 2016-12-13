6704cb352a59d797023c9e43c85ae9dd19a65244
Leonardo <ldrsjoao@inf.ufpel.edu.br>
2016-12-13 11:28:30 -0200

    criando parte gráfica

--- aprendendo.py
+++ aprendendo.py
@@ -9,17 +9,63 @@ class Kanvas:
         # self.canvas2 = Canvas(raiz, width=200, height=200, cursor='dot', bd=5, bg='purple')
         # self.canvas2.pack(side=LEFT)
         # #self.canvas2["text"] = "Autenticado"
-        w = Label(root, text="red", bg="red", fg="white")
-        w.pack(padx=5, pady=10, side=LEFT)
-        w = Label(root, text="green", bg="green", fg="black")
-        w.pack(padx=5, pady=20, side=LEFT)
-        w = Label(root, text="blue", bg="blue", fg="white")
-        w.pack(padx=5, pady=20, side=LEFT)
+        self.fontePadrao = ("Arial", "10")
 
-        self.juca = Label(root, text="", bg="blue", fg="white", width=50)
-        self.juca.pack(padx=5, pady=30, side=LEFT)
 
-        self.juca['text'] = "Aleluia, consegui fazer o quero!!!!\n juca"
+        self.primeiroContainer = Frame(root)
+        self.primeiroContainer["pady"] = 10
+        self.primeiroContainer["padx"] = 150
+        self.primeiroContainer.pack()
+
+        self.segundoContainer = Frame(root)
+        self.segundoContainer["pady"] = 20
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(root)
+        self.terceiroContainer["pady"] = 30
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(root)
+        self.quartoContainer["pady"] = 60
+        self.quartoContainer.pack()
+
+        #------------------------------------------------------------------------------------
+        self.juju = Label(self.primeiroContainer, text="CHAT - Bem Vindo")
+        self.juju["font"] = self.fontePadrao
+        self.juju.pack()
+
+        self.conversaLabel = Label(self.segundoContainer, text="Conversa", font=self.fontePadrao, width=90)
+        self.conversaLabel.pack(padx=10, side=LEFT)
+
+        self.loginLabel = Label(self.segundoContainer, text="Pessoas Logadas", font=self.fontePadrao, width=50)
+        self.loginLabel.pack(padx=100, side=LEFT)
+
+        # w = Label(self.terceiroContainer, text="red", bg="red", fg="white")
+        # w.pack(pady=10, side=LEFT)
+        # w = Label(self.terceiroContainer, text="green", bg="green", fg="black")
+        # w.pack(pady=20, side=LEFT)
+        # w = Label(self.terceiroContainer, text="blue", bg="blue", fg="white")
+        # w.pack(pady=30, side=LEFT)
+
+        self.conversa = Label(self.terceiroContainer, text="", bg="blue", width=90, height=30)
+        self.conversa.pack(padx=0, side=LEFT)
+
+        self.login = Label(self.terceiroContainer, text="", bg="red", width=50, height=30)
+        self.login.pack(padx=100, side=LEFT)
+
+        # self.teste = Label(self.terceiroContainer, text="lzsdklasjasj")
+        # self.teste.pack(padx=10, side=LEFT)
+        # self.teste = Label(self.terceiroContainer, text="jucaaaaaaaaa")
+        # self.teste.pack(padx=40, side=LEFT)
+
+        # self.juca = Label(self.quartoContainer, text="", bg="blue", fg="white", width=150)
+        # self.juca.pack(side=LEFT)
+        #
+        # self.set_text("akjsdklajksd", self.juca)
+
+    def set_text(self, master=None, lab=None):
+
+        lab['text'] = master
 
 instancia=Tk()
 Kanvas(instancia)
